34e82fb3bd96a5b15ba7f79d101b3a870d578a77
David Akinola <dakinola54@gmail.com>
2025-01-06 22:50:29 +0100

    implemented login, logout and signup functionality

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
 from .models import CustomUser
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from .forms import CustomUserCreationForm
 
 
 class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
     model = CustomUser
     success_url = reverse_lazy('login')
