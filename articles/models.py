2614d6f49b634e18a5f979bc6815271cbbba898e
David Akinola <dakinola54@gmail.com>
2025-01-08 20:59:05 +0100

    database fields

--- articles/models.py
+++ articles/models.py
@@ -3,3 +3,14 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 
+class Articles(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE
+    )
+    date = models.DateTimeField(auto_now_add=True)
+
+
+    
